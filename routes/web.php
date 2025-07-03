58906a94ef3d69a5f12f52abc7642bc0ec3ada85
jalalkhan1011 <jalal.khan981@gmail.com>
2025-07-03 16:06:31 +0600

    route & controller

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello',function(){
+    return "Hello From KhansIT";
+});
+
+// Route::get('/home',function(){
+//     return view('home');
+// });
+
+Route::get('/home',[HomeController::class,'index']);
+
+// Route::get('/user/{name}',function($name){
+//     return "User name is:$name";
+// });
+
+Route::get('/user/{name}',[HomeController::class,'show']);
