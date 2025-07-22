1db3572b4c41bac81ef3642841aa82f1944d1783
jalalkhan1011 <jalal.khan981@gmail.com>
2025-07-22 15:49:30 +0600

    authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showRegisterForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect(route('login'))->with('success', 'Registration successful! Please log in.');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return redirect(route('home'))->with('success', 'Login Successfully!');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid credentials.',
+        ]);
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect(route('login'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,42 +6,25 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\StudentController;
-
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/form',[FormController::class,'showForm']);
-// Route::post('/form',[FormController::class,'submit'])->name('submit.form');
-/////
-// Route::get('/students',[StudentController::class,'index'])->name('student.index');
-// Route::get('/student/create',[StudentController::class,'create'])->name('student.create');
-// Route::post('/student/store',[StudentController::class,'store'])->name('student.store');
-// Route::get('/student/show/{id}',[StudentController::class,'show'])->name('student.show');
-// Route::get('/student/{id}/edit',[StudentController::class,'edit'])->name('student.edit');
-// Route::put('/student/update/{id}',[StudentController::class,'update'])->name('student.update');
-// Route::delete('/student/delete/{id}',[StudentController::class,'delete'])->name('student.delete');
+Route::get('/register',[AuthController::class,'showRegisterForm'])->name('register');
+Route::post('/register',[AuthController::class,'register']);
+Route::get('/login',[AuthController::class,'showLoginForm'])->name('login');
+Route::post('/login',[AuthController::class,'login']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('auth')->name('home');
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
-Route::get('/students/{student}',[StudentController::class,'show'])->name('students.show');
-Route::resource('/students', StudentController::class);
+// Auth::routes();
 
-// Route::get('/cards',[HomeController::class,'card']);
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('/hello',function(){
-//     return "Hello From KhansIT";
-// });
-
-// Route::get('/home',function(){
-//     return view('home');
-// });
-
-// Route::get('/home',[HomeController::class,'index']);
-
-// Route::get('/user/{name}',function($name){
-//     return "User name is:$name";
-// });
-
-// Route::get('/user/{name}',[HomeController::class,'show']);
-
-// Route::resource('/products',ProductController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
+    Route::resource('/students', StudentController::class);
+});
